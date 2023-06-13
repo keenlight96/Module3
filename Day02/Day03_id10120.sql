632c2ade39983499e3d63623ea42597e5d09fcb5
keenlight96 <nguyenkimlong29196@gmail.com>
2023-06-13 15:21:14 +0700

    1

--- Day02/Day03_id10120.sql
+++ Day02/Day03_id10120.sql
@@ -34,7 +34,26 @@ CREATE TABLE Mark(
     FOREIGN KEY (StudentID) REFERENCES Student(StudentID)
 );
 
+insert into Class(ClassName, StartDate, Status) values 
+("A1","2008-12-20",1),
+("A2","2008-12-22",1),
+("B3",current_date(),0);
 
+insert into Student (StudentName, Address, Phone, Status, ClassID) values
+("Hung", "Ha Noi", "0912113113", 1, 1),
+("Hoa", "Hai Phong", "", 1, 1),
+("Manh", "HCM", "0123123123", 0, 2);
+
+insert into Subject (SubName, Credit, Status) values
+("CF", 5, 1),
+("C", 6, 1),
+("HDJ", 5, 1),
+("RDBMS", 10, 1);
+
+insert into Mark (SubID, StudentID, Mark, ExamTimes) values
+(1, 1, 8, 1),
+(1, 2, 10, 2),
+(2, 1, 12, 1);
 
 
 
