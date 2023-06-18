222bb008ca4fa369301a865a2882602b2ddf16e6
keenlight96 <nguyenkimlong29196@gmail.com>
2023-06-18 18:03:59 +0700

    1

--- Day04/Day04_id10123.sql
+++ Day04/Day04_id10123.sql
@@ -16,3 +16,8 @@ select Student.StudentID, Student.StudentName, avg(Mark.Mark) as AverageMark fro
 join Mark on Mark.StudentID = Student.StudentID
 group by Student.StudentID
 having AverageMark >= all (select avg(mark) from Mark group by StudentID);
+
+select Student.StudentID, Student.StudentName, avg(Mark.Mark) as AverageMark from Student
+join Mark on Mark.StudentID = Student.StudentID
+group by Student.StudentID
+having AverageMark = (select max(Max1) from (select studentId, avg(Mark) as Max1 from Mark group by StudentId) as temp);
